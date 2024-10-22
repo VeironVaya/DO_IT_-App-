14d0c718b5ca565fd24c72de9d24b72ad81297cc
Veiron Vaya Yarief <veironvayayarief41@gmail.com>
2024-10-22 16:41:54 +0700

    Update dialog_box.dart

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,23 +1,53 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:first_app/util/dialog_box_button.dart';
 import 'package:flutter/material.dart';
 
-class DialogBoxButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed; // Use final
-
-  DialogBoxButton({
-    super.key,
-    required this.text,
-    required this.onPressed,
-  });
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+  DialogBox(
+      {super.key,
+      required this.controller,
+      required this.onSave,
+      required this.onCancel});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: onPressed,
-      color: Colors.black,
-      child: Text(
-        text,
-        style: TextStyle(color: Colors.orange),
+    return AlertDialog(
+      backgroundColor: Colors.orange,
+      content: Container(
+        height: 160,
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: "Add New Activity",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50, left: 60, right: 10),
+              child: Row(
+                children: [
+                  // Save button
+                  DialogBoxButton(text: "Save", onPressed: onSave),
+                  // Space between
+                  SizedBox(width: 5),
+                  // Cancel button
+                  DialogBoxButton(text: "Cancel", onPressed: onCancel),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
